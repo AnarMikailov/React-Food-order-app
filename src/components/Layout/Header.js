a3f16b074cf10c736734d7202fe51df40b704490
AnarMikailov <a.v.mikayilov@gmail.com>
2022-11-17 18:04:12 +0400

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import mealsimage from "../../assets/meals.jpg";
+import HeadCartButton from "./HeadCartButton";
+import styles from "./Header.module.css";
+function Header(props) {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1>React Meals</h1>
+        <HeadCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsimage} alt="A table delicious food" />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
